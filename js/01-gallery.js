9b22dedb39548720427c4b9cd3e4ef0d547b8286
Rummut <kzantip@gmail.com>
2023-06-12 02:06:09 +0300

    finish all tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markUp = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", markUp);
+
+galleryEl.addEventListener("click", (event) => {
+    console.log(event.target)
+    event.preventDefault()
+    basicLightbox.create(`
+		<img width="100%" height="100%" class="gallery__image__box" src="${event.target.dataset.source}">
+	`).show()
+})
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markUp = galleryItems
+  .map(
+      ({ preview, original, description }) => {
+          const galleryItems = document.createElement('li')
+          galleryItems.classList.add("gallery__item")
+          galleryEl.append(galleryItems)
+
+          const galleryLink = document.createElement('a')
+          galleryLink.classList.add("gallery__link")
+          galleryLink.href = `${original}`
+          galleryItems.append(galleryLink)
+
+          const image = document.createElement('img')
+          image.classList.add("gallery__image")
+          image.src = `${preview}`
+          image.alt = `${description}`
+          galleryLink.append(image)
+    
+      }
+  )
+  .join("");
+  
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250});
+gallery.on('show.simplelightbox')
